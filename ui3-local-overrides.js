a38b25f6cce5cf09937e38fc20c4d4925d89784e
Sebastian Malek <malek@malek.li>
2022-02-13 19:37:43 +0100

    Create ui3-local-overrides.js

---
+++ ui3-local-overrides.js
@@ -0,0 +1,8 @@
+OverrideDefaultSetting("ui3_timeout",            "0",    true,            false,          1);
+OverrideDefaultSetting("ui3_time24hour",         "1",    true,            false,          1);
+
+OverrideDefaultSetting("ui3_pause_when_hidden",                "0",          true,            false,          1);
+
+// Category "Streaming Profiles"
+//                     Settings Key                 Value                                                                                                                                                                                                                                                                                                                                                                                                                                                        Options Window   Always Reload   Generation
+OverrideDefaultSetting("ui3_streamingProfileArray", "[{\"dv\":4,\"name\":\"1080p^\",\"abbr\":\"2MP\",\"aClr\":\"#004882\",\"vcodec\":\"h264\",\"stream\":0,\"w\":1920,\"h\":1080,\"q\":-1,\"limitBitrate\":2,\"kbps\":2048,\"fps\":-1,\"gop\":-1,\"zfl\":-1,\"pre\":-1,\"pro\":-1},{\"dv\":4,\"name\":\"720p^\",\"abbr\":\"1MP\",\"aClr\":\"#003862\",\"vcodec\":\"h264\",\"stream\":0,\"w\":1280,\"h\":720,\"q\":-1,\"limitBitrate\":2,\"kbps\":1024,\"fps\":-1,\"gop\":-1,\"zfl\":-1,\"pre\":-1,\"pro\":-1}]", true,            false,          1);
